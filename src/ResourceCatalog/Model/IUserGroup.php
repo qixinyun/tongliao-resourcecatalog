2693cc0566235e9654777724a25c590b7f652b5d
Leon <apple@apples-MacBook-Pro.local>
2019-09-04 17:55:04 +0800

    init dev

---
+++ src/ResourceCatalog/Model/IUserGroup.php
@@ -0,0 +1,49 @@
+<?php
+namespace ResourceCatalog\Model;
+
+interface IUserGroup extends IUserGroupIdentify
+{
+    const ID = array(
+        'NULL' => 0,
+        'FGW' => 1,
+        'JYJ' => 2,
+        'KJJ' => 3,
+        'JXW' => 4,
+        'MZSWWYH' => 5,
+        'GAJ' => 6,
+        'MZJ' => 7,
+        'SFJ' => 8,
+        'CZJ' => 9,
+        'RSJ' => 10,
+        'GZW' => 11,
+        'GHJ' => 12,
+        'ZJJ' => 13,
+        'HBJ' => 14,
+        'JTJ' => 15,
+        'SWJ' => 16,
+        'NMJ' => 17,
+        'LCJ' => 18,
+        'SHANGWJ' => 19,
+        'WGJ' => 20,
+        'WJW' => 21,
+        'SJJ' => 22,
+        'TYJ' => 23,
+        'SYJ' => 24,
+        'GSJ' => 25,
+        'ZJJ' => 26,
+        'AJJ' => 27,
+        'TJJ' => 28,
+        'LFW' => 29,
+        'LSJ' => 30,
+        'JRB' => 31,
+        'RF' => 32,
+        'FPB' => 33,
+        'SHUIWJ' => 34,
+        'CRJJYJYJ' => 35,
+        'QXJ' => 36,
+        'HG' => 37,
+        'YZJ' => 38,
+        'YWJ' => 39,
+        'TLSYCJ' => 40
+    );
+}
